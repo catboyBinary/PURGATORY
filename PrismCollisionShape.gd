1108b5bdd67c883e0b034eaeea960d953fd7d27c
catboybinary <b1n4ry@guren.ru>
2023-04-25 18:24:46 +0300

    HE'S ONTO SOMETHING?????????????

--- PrismCollisionShape.gd
+++ PrismCollisionShape.gd
@@ -10,10 +10,15 @@ extends CollisionShape3D
 @export var upper_radius = 10
 @export_range(0, 45) var lower_angle = 50
 @export var lower_radius = 10
-
-func _ready() -> void:
-	shape = ConvexPolygonShape3D.new()
-	for i in range(sides_count):
-		print('a')
-		var angle = 2 * PI * (i/sides_count)
-		shape.points.append(Vector3(cos(angle),0,sin(angle))) 
+@export_group("IN EDITOR")
+@export var start: bool
+func _process(delta: float):
+	if start:
+		shape = ConvexPolygonShape3D.new()
+		var points : PackedVector3Array
+		for i in range(1, sides_count+1):
+			var angle : float = deg_to_rad(i*(360/sides_count))
+			points.append(Vector3(radius*cos(angle),0,radius*sin(angle)))
+		shape.points = points	
+		print(shape.points)
+		start = false
