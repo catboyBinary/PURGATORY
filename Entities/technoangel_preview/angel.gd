ef193745d44a7281ce66831fdf22e34c813d7af3
catboybinary <b1n4ry@guren.ru>
2023-04-26 00:19:04 +0300

    Cock'n'Balls™️ rotation hotfix

--- Entities/technoangel_preview/angel.gd
+++ Entities/technoangel_preview/angel.gd
@@ -6,5 +6,12 @@ var wing2
 func _process(delta):
 	wing1 = get_parent().get_node("wing1")
 	wing2 = get_parent().get_node("wing2")
-	wing1.position = wing1.position.lerp(position+Vector3(1,1,1),delta*7)
-	wing2.position = wing2.position.lerp(position+Vector3(-1,1,1),delta*7)
+	wing1.position = wing1.position.lerp(position+rotated_as(Vector3(1,1,-1),rotation),delta*7)
+	wing2.position = wing2.position.lerp(position+rotated_as(Vector3(-1,1,-1),rotation),delta*7)
+
+func rotated_as(vector: Vector3, rotation: Vector3) -> Vector3:
+	var rotated_vector = vector
+	rotated_vector = rotated_vector.rotated(Vector3.BACK,rotation.z)
+	rotated_vector = rotated_vector.rotated(Vector3.UP,rotation.y)
+	rotated_vector = rotated_vector.rotated(Vector3.RIGHT,rotation.x)
+	return rotated_vector
